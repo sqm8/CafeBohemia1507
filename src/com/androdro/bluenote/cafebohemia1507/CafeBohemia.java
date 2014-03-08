e0b3f1395c405e38679f6948dc0ed61ffdd15cb8
Hiro Sakuma <sakuma@zero52.com>
2014-03-08 09:41:08 +0900

    first commit

---
+++ src/com/androdro/bluenote/cafebohemia1507/CafeBohemia.java
@@ -0,0 +1,15 @@
+package com.androdro.bluenote.cafebohemia1507;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CafeBohemia extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
